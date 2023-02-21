cdb4a2d1768fb82e2d3a1b982566c8e9f4f887d9
SaraCS21 <saracs15.scssn@gmail.com>
2023-02-21 11:53:50 +0000

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import logo from "@assets/logo.png"
 
 const Header = () => {
     return (
-        <header className='w-full flex justify-center items-center bg-slate-300'>
-            <nav>
-                <p>Producto</p>
+        <header className='w-full flex justify-start items-center p-6 bg-slate-300'>
+            <nav className='w-1/4 flex flex-col items-center'>
+                <img src={logo} alt="logo" />
+                <p>Product Shop</p>
             </nav>
         </header>
     )
