671ade595e2b93f73d5d85b10d1b4fd54c16bf75
SaraCS21 <saracs15.scssn@gmail.com>
2023-02-21 12:33:38 +0000

    modificando header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import logo from "@assets/logo.png"
+import React from 'react';
+import logo from "@assets/logo.png";
+import { BiSearch } from 'react-icons/bi';
 
 const Header = () => {
     return (
-        <header className='w-full flex justify-start items-center p-6 bg-slate-300'>
-            <nav className='w-1/4 flex flex-col items-center'>
+        <header className='w-full flex justify-center items-center p-6 bg-slate-300'>
+            <nav className='w-1/4 flex justify-center items-center'>
                 <img src={logo} alt="logo" />
-                <p>Product Shop</p>
+                <div className='w-2/4 flex'>
+                    <input className='w-full p-1' name="search" placeholder='Busca un producto...' />
+                    <div className='p-2 bg-blue-600 text-white'>
+                        <BiSearch />
+                    </div>
+                    <div></div>
+                </div>
             </nav>
         </header>
     )
