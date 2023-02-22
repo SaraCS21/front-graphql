d312954dd874a3dc6e219c0cdfe6475d5979d603
Sstark97 <aitorscinfo@gmail.com>
2023-02-22 10:26:08 +0000

    Add responsive to users

--- src/components/UserItem.jsx
+++ src/components/UserItem.jsx
@@ -3,7 +3,7 @@ const UserItem = ({ user }) => {
     const { avatar, email, name, role } = user
 
     return (
-        <li className="w-[19%] flex flex-col justify-between border border-slate-300 rounded shadow p-5 mt-4 mr-3 hover:cursor-pointer">
+        <li className="w-5/6 lg:w-[19%] flex flex-col justify-between border border-slate-300 rounded shadow p-5 mt-4 mr-3 hover:cursor-pointer">
             <img className="w-[90%] h-48 self-center" src={avatar} alt={name} />
             <div className="p-3">
                 <p className="text-purple font-medium">{role}</p>
